c0137632f2d2cabe9adb2afa4aba557dfdc9ba61
Michael Watts <wattsjmichael@gmail.com>
2020-09-09 14:39:15 -0700

    adds search and images and seperate promise logic
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

--- src/main.js
+++ src/main.js
@@ -2,31 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import NasaService from './nasa-service.js';
 
 $(document).ready(function () {
   $("#marsButton").click(function () {
     const earthDate = $("#marsFind").val();
-    $("#marsFind").val(""); //This might be an aesthitic<sp> thing;
+    $("#marsFind").val(""); 
     $("#marsResult").show();
-
-    let request = new XMLHttpRequest();
-    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${earthDate}&api_key=${process.env.API_KEY}`;
-
-    request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
-    
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response) {
-      $("#searchResultOne").attr("src", response.photos[0].img_src);
-      $("#searchResultOneText").html("Camera: " + response.photos[0].camera.name + "<br>" + "Rover: " + response.photos[0].rover.name);
-      $("#searchResultTwo").attr("src", response.photos[3].img_src);
-      $("#searchResultTwoText").html("Camera: " + response.photos[3].camera.name + "<br>" + "Rover: " + response.photos[0].rover.name);
-    }
+    let promise= NasaService.getMars(earthDate);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $("#searchResultOne").attr("src", body.photos[0].img_src);
+      $("#searchResultOneText").html("Camera: " + body.photos[0].camera.name + "<br>" + "Rover: " + body.photos[0].rover.name);
+      $("#searchResultTwo").attr("src", body.photos[3].img_src);
+      $("#searchResultTwoText").html("Camera: " + body.photos[3].camera.name + "<br>" + "Rover: " + body.photos[0].rover.name);
+    }, function(error) {
+      $("#searchResultOneText").html(`ERROR! Call Jeff! ${error}`);
+    });
   });
 }); 
---
+++ src/nasa-service.js
@@ -0,0 +1,17 @@
+export default class NasaService {
+  static getMars(earthDate){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${earthDate}&api_key=${process.env.API_KEY}`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
